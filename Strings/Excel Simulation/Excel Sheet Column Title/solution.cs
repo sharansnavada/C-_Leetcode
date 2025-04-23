4e3aa26a248878082475609a48a6487eacb51f0d
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-04-23 22:11:46 +0530

    Update solution.cs

--- Strings/Excel Simulation/Excel Sheet Column Title/solution.cs
+++ Strings/Excel Simulation/Excel Sheet Column Title/solution.cs
@@ -1,1 +1,17 @@
 
+public class Solution {
+    public string ConvertToTitle(int columnNumber) {
+        string result = "";
+        
+        while (columnNumber > 0) {
+            columnNumber--; // Adjust to 0-based index
+            int remainder = columnNumber % 26;
+            char letter = (char)(remainder + 'A');
+            result = letter + result;
+            columnNumber /= 26;
+        }
+        
+        return result;
+        
+    }
+}
