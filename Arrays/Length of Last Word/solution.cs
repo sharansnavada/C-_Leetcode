31a1e1e3c0fc5f3f651ac9af6069e35c01961d37
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-04-23 22:01:11 +0530

    Create solution.cs

---
+++ Arrays/Length of Last Word/solution.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        s = s.TrimEnd(' ');
+        string[] arr = s.Split(' ');
+        return arr[arr.Length - 1].Length;
+    }
+}
