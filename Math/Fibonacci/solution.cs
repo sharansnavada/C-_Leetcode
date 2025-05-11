e13a7d6695d87ea9a969bae264d133a71e0f14ce
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-05-11 23:18:21 +0530

    Create solution.cs

---
+++ Math/Fibonacci/solution.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int Fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
