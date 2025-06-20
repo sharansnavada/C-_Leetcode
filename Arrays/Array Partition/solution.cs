44b95f3fe068405e5e293ff870af15c7b7d7dcc6
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-06-20 22:47:51 +0530

    Create solution.cs
    Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
    
     
    
    Example 1:
    
    Input: nums = [1,4,3,2]
    Output: 4
    Explanation: All possible pairings (ignoring the ordering of elements) are:
    1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
    2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
    3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
    So the maximum possible sum is 4.
    Example 2:
    
    Input: nums = [6,2,6,5,1,2]
    Output: 9
    Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
     
    
    Constraints:
    
    1 <= n <= 104
    nums.length == 2 * n
    -104 <= nums[i] <= 104

---
+++ Arrays/Array Partition/solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int ArrayPairSum(int[] nums) {
+        int sum = 0;
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length; i = i+2){
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
