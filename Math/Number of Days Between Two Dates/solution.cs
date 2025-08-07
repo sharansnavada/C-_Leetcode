12646da254cfdb3d8462db34158740da9ed1820a
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-08-08 00:15:24 +0530

    Update solution.cs
    Added full logic, but there is many logical errors that I will look into this tomorrow

--- Math/Number of Days Between Two Dates/solution.cs
+++ Math/Number of Days Between Two Dates/solution.cs
@@ -1,16 +1,56 @@
-public class solution
+public class nod
     {
         Dictionary<int, int> days = new Dictionary<int, int>(){{1,31},{3,31},{4,30},{5,31},{6,30},{7,31},{8,31},
                                                             {9,30},{10,31},{11,30},{12,31}};
         int numberOfDays = 0;
         int year1 = 0; int year2 = 0;
         int month1 = 0; int month2 = 0;
-        int firstDate = 0; int secondDate2 = 0;
+        int firstDate = 0; int secondDate = 0;
 
         public int DaysBetweenDates(string date1, string date2)
         {
-            AssignSplittedData(date1, year1, month1, firstDate);
-            AssignSplittedData(date2, year2, month2, secondDate2);
+        //AssignSplittedData(date1, year1, month1, firstDate);
+        //AssignSplittedData(date2, year2, month2, secondDate);
+            string[] splitData = date1.Split('-');
+            year1 = int.Parse(splitData[2]);
+            month1 = int.Parse(splitData[1]);
+            firstDate = int.Parse(splitData[0]);
+            splitData = date2.Split('-');
+            year2 = int.Parse(splitData[2]);
+            month2 = int.Parse(splitData[1]);
+            secondDate = int.Parse(splitData[0]);
+            CalculateYear();
+            return numberOfDays;
+        }
+
+        public void CalculateYear(){
+            // while(year1 != year2 - 1){
+            //     if(IsLeapYear(year1)){
+            //         numberOfDays += 366;
+            //     }
+            //     else{
+            //         numberOfDays += 365;
+            //     }
+            //     year1++;
+            // }
+
+            if(year1 == year2){
+                while(month1 != month2){
+                    numberOfDays += DaysInGivenMonth(month1,year1);
+                    month1++;
+                    if(month1 == 12){
+                        month1++; year1++;
+                    }
+                }
+                numberOfDays = NumberOfDays(firstDate,secondDate,numberOfDays);
+            }
+            else if(year1 == year2 - 1){
+                int numberOfMonths = (month1 + month2) % 12 + 12;
+                while(numberOfMonths != 0){
+                    numberOfDays += days[month1];
+                    month1++; numberOfMonths--;
+                }
+            }
         }
 
         public void AssignSplittedData(string date, int year, int month, int Date)
