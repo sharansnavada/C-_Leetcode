db1d99797dda841da507248afbc60e33158b9ab4
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-08-07 18:20:42 +0530

    Update solution.cs
    Added a method to assign splitted data.

--- Math/Number of Days Between Two Dates/solution.cs
+++ Math/Number of Days Between Two Dates/solution.cs
@@ -1,59 +1,68 @@
 public class solution
-{
-    Dictionary<int, int> days = new Dictionary<int, int>(){{1,31},{3,31},{4,30},{5,31},{6,30},{7,31},{8,31},
+    {
+        Dictionary<int, int> days = new Dictionary<int, int>(){{1,31},{3,31},{4,30},{5,31},{6,30},{7,31},{8,31},
                                                             {9,30},{10,31},{11,30},{12,31}};
-    int numberOfDays = 0;
-    int year1 = 0; int year2 = 0;
-    int month1 = 0; int month2 = 0; 
-    int firstDate = 0; int secondDate2 = 0;
-    
-    public int DaysBetweenDates(string date1, string date2)
-    {
+        int numberOfDays = 0;
+        int year1 = 0; int year2 = 0;
+        int month1 = 0; int month2 = 0;
+        int firstDate = 0; int secondDate2 = 0;
 
-    }
-
-    public int DaysInGivenMonth(int month, int year)
-    {
-        if (month == 2)
+        public int DaysBetweenDates(string date1, string date2)
         {
-            if (IsLeapYear(year))
-                return 29;
+            AssignSplittedData(date1, year1, month1, firstDate);
+            AssignSplittedData(date2, year2, month2, secondDate2);
+        }
+
+        public void AssignSplittedData(string date, int year, int month, int Date)
+        {
+            string[] splitData = date.Split('-');
+            year = int.Parse(splitData[0]);
+            month = int.Parse(splitData[1]);
+            Date = int.Parse(splitData[2]);
+        }
+
+        public int DaysInGivenMonth(int month, int year)
+        {
+            if (month == 2)
+            {
+                if (IsLeapYear(year))
+                    return 29;
+                else
+                    return 28;
+            }
             else
-                return 28;
+                return days[month];
         }
-        else
-            return days[month];
-    }
 
-    public int NumberOfDays(int date1, int date2, int numberOfDays)
-    {
-        if (date1 > date2)
+        public int NumberOfDays(int date1, int date2, int numberOfDays)
         {
-            while (date1 != date2)
+            if (date1 > date2)
             {
-                numberOfDays--;
-                date1--;
+                while (date1 != date2)
+                {
+                    numberOfDays--;
+                    date1--;
+                }
             }
-        }
-        else if (date1 < date2)
-        {
-            while (date1 != date2)
+            else if (date1 < date2)
             {
-                numberOfDays++;
-                date1++;
+                while (date1 != date2)
+                {
+                    numberOfDays++;
+                    date1++;
+                }
             }
+            return numberOfDays;
         }
-        return numberOfDays;
-    }
 
-    public bool IsLeapYear(int year)
-    {
-        if (year % 4 == 0)
+        public bool IsLeapYear(int year)
         {
-            if (year % 100 == 0)
-                return year % 400 == 0;
-            return true;
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                    return year % 400 == 0;
+                return true;
+            }
+            return false;
         }
-        return false;
     }
-}
