c367e04daf14176745e3f98110f9212679c79c60
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2025-09-03 22:01:02 +0530

    Create solution.cs
    Given a square matrix mat, return the sum of the matrix diagonals.
    
    Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
    
     
    
    Example 1:
    
    
    Input: mat = [[1,2,3],
                  [4,5,6],
                  [7,8,9]]
    Output: 25
    Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
    Notice that element mat[1][1] = 5 is counted only once.
    Example 2:
    
    Input: mat = [[1,1,1,1],
                  [1,1,1,1],
                  [1,1,1,1],
                  [1,1,1,1]]
    Output: 8
    Example 3:
    
    Input: mat = [[5]]
    Output: 5
     
    
    Constraints:
    
    n == mat.length == mat[i].length
    1 <= n <= 100
    1 <= mat[i][j] <= 100

---
+++ Matrix/Jagged Array/Matrix Diagonal Sum/solution.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int DiagonalSum(int[][] mat) {
+        int fromLeft = 0;
+        int fromRight = mat.Length - 1;
+        int sum = 0;
+        
+        while(fromLeft < mat.Length && fromRight >= 0){
+            if(fromLeft == fromRight) //if the row's length is odd number, then I'm preventing middle number to add twice 
+                sum += mat[fromLeft][fromLeft]; 
+            else{
+                sum += mat[fromLeft][fromLeft] + mat[fromLeft][fromRight];
+            }
+            fromLeft++; fromRight--;
+        }
+        return sum;
+    }
+}
