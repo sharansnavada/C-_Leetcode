cc556933e919a89fd593352690d1224d5dfc165e
SNavada6801 <snavada@lnw.com>
2025-11-05 16:01:25 +0530

    Added proper logic
    Fixed index out of bound error and corrected the code

--- Dynamic Programming/Minimum Time to Make Rope Colorful/solution.cs
+++ Dynamic Programming/Minimum Time to Make Rope Colorful/solution.cs
@@ -1,22 +1,32 @@
 //MinCost("aaabbbabbbb", new int[] {3,5,10,7,5,3,5,5,4,8,1});
 //output - 26
+//MinCost("aabaa", new int[] {1,2,3,4,1});
+//output - 2
 public class Solution {
     public int MinCost(string colors, int[] neededTime) {
+        if (colors.Length == 1)
+            return 0;
         int minTime = 0;
         char prevColor = colors[0];
         int subCount = 0;
         int maxTime = int.MinValue;
 
-        for(int i = 1; i < colors.Length; i++){
-            if(i == colors.Length - 1 && colors.Length > 1){
-                if(colors[i - 1] == colors[i]){
-                    subCount += neededTime[i];
-                    maxTime = maxTime < neededTime[i] ? neededTime[i] : maxTime;
+        for(int i = 1; i <= colors.Length; i++){
+            if (i == colors.Length && colors.Length > 1)
+            {
+                if (colors[i - 1] == colors[i - 2])
+                {
+                    subCount += neededTime[i - 1];
+                    maxTime = maxTime < neededTime[i - 1] ? neededTime[i - 1] : maxTime;
                     minTime += subCount - maxTime;
                     break;
                 }
+                else
+                    break;
             }
+
             char currentColor = colors[i];
+
             if(currentColor == prevColor){
                 subCount += neededTime[i - 1];
                 maxTime = maxTime < neededTime[i - 1] ? neededTime[i - 1] : maxTime;
@@ -28,9 +38,7 @@ public class Solution {
                 subCount = 0; maxTime = int.MinValue;
 
             }
-            if( i == colors.Length - 1 && prevColor == currentColor){
 
-            }
             prevColor = currentColor;
         }
 
