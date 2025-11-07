a297a80d0b9a28e0fb541ecfea012b73f3367cd7
sharansnavada <sharansnaavada@gmail.com>
2025-11-07 19:11:15 +0530

    Update solution.cs
    Updated the logic to fix issue

--- HashTable/Next Greater Element I/solution.cs
+++ HashTable/Next Greater Element I/solution.cs
@@ -2,29 +2,24 @@ public class Solution {
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
         int[] retArr = new int[nums1.Length];
         int[] hashTable = new int[100002];
-        bool hasGot = false;
 
-        for(int i = 0; i < nums1.Length; i++){
-            hasGot = false;
-            for(int j = 0; j < nums2.Length; j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k = j + 1; k < nums2.Length; j++){
-                        if(nums2[k] > nums2[j]){
-                            hashTable[nums2[j]] = nums2[k];
-                            hasGot = true;
-                            break;
-                        }
-                    }
-                }
-                if(hasGot){
+        for (int i = 0; i < hashTable.Length; i++) {
+            hashTable[i] = -1;
+        }
+
+        for (int i = 0; i < nums2.Length; i++) {
+            for (int j = i + 1; j < nums2.Length; j++) {
+                if (nums2[j] > nums2[i]) {
+                    hashTable[nums2[i]] = nums2[j];
                     break;
                 }
             }
         }
 
-        for(int l = 0; l < nums1.Length; l++){
+        for (int l = 0; l < nums1.Length; l++) {
             retArr[l] = hashTable[nums1[l]];
         }
+
         return retArr;
     }
 }
