e016ca065a71a4e0acfd05a434b43b79cb8da83a
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2026-04-17 23:10:39 +0530

    Refactor MinMirrorPairDistance method and remove unused code

--- Math/Minimum Absolute Distance Between Mirror Pairs/solution.cs
+++ Math/Minimum Absolute Distance Between Mirror Pairs/solution.cs
@@ -1,54 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+public class Solution {
+    public int MinMirrorPairDistance(int[] nums) {
+        int n = nums.Length;
+        int ans = n + 1;
+        var pos = new Dictionary<int, int>();
 
-namespace C__Projects
-{
-    public class Solution
-    {
-        public int MinMirrorPairDistance(int[] nums)
-        {
-            int length = nums.Length;
-            int[] reversedNums = ReverseArrayElement(nums, length);
-            int minDistance = int.MaxValue;
+        for (int i = 0; i < n; i++) {
+            int v = nums[i];
 
-            for(int i = 0; i < length; i++)
-            {
-                for(int j = i + 1; j < length; j++)
-                {
-                    if (reversedNums[i] == nums[j])
-                    {
-                        minDistance = Math.Abs(i - j) < minDistance ? Math.Abs(i - j) : minDistance;
-                    }
-                }
-            }
+            if (pos.TryGetValue(v, out int j))
+                ans = Math.Min(ans, i - j);
 
-            return minDistance == int.MaxValue ? -1 : minDistance;
+            int rev = Reverse(v);
+            // Always overwrite: latest index = closest future right endpoint
+            pos[rev] = i;
         }
 
-        public int[] ReverseArrayElement(int[] num, int length)
-        {
-            int[] retArr = new int[length];
-            int index = 0;
+        return ans > n ? -1 : ans;
+    }
 
-            foreach (int number in num)
-            {
-                StringBuilder sb = new StringBuilder();
-                int currNumber = number;
-                while (currNumber > 0)
-                {
-                   // sb.Insert(0, (currNumber % 10).ToString());
-                    sb.Append((currNumber % 10).ToString());
-                    currNumber = currNumber / 10;
-                }
-                retArr[index++] = int.Parse(sb.ToString().TrimStart('0'));
-
-            }
-            return retArr;
-
+    private static int Reverse(int x) {
+        int y = 0;
+        while (x > 0) {
+            y = y * 10 + x % 10;
+            x /= 10;
         }
+        return y;
     }
 }
-
