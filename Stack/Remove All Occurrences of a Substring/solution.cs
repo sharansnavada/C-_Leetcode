b9e9032965752da1325a4cecaebaea119f054e4c
sharansnavada <96814407+sharansnavada@users.noreply.github.com>
2026-08-21 20:31:42 +0530

    Refactor comment and enhance code structure
    Corrected comment formatting and improved code readability.

--- Stack/Remove All Occurrences of a Substring/solution.cs
+++ Stack/Remove All Occurrences of a Substring/solution.cs
@@ -1,4 +1,4 @@
-//Worst structured code ever written by the human race just to pass all test cases.
+// Worst structured code ever written by the human race just to pass all the test cases.
 
 public class Solution {
     public string RemoveOccurrences(string s, string part) {
@@ -7,10 +7,13 @@ public class Solution {
         for(int i = 0; i < s.Length; i++){
             char currLetter = s[i];
             bool falseAlarm = false;
+            
             stk.Push(currLetter);
+            
             if(currLetter == part[part.Length - 1] && stk.Count >= part.Length){
                 int partIndex = part.Length - 1;
                 StringBuilder sb = new StringBuilder();
+                
                 while(partIndex > -1 && stk.Count > 0){
                     char l = stk.Pop();
                     sb.Append(l);
@@ -20,19 +23,24 @@ public class Solution {
                     }
                     partIndex--;
                 }
+                
                 if(falseAlarm){
                     string strSB = new string(sb.ToString().Reverse().ToArray());
+                    
                     for(int j = 0; j < strSB.ToString().Length; j++){
                         stk.Push(strSB.ToString()[j]);
                     }
                 }
             }
         }
+        
         StringBuilder sbb = new StringBuilder();
+        
         while(stk.Count != 0)
         {
             sbb.Append(stk.Pop());
         }
+        
         return new string(sbb.ToString().Reverse().ToArray());
     }
 }
